438c3c6011c7f888cc90da8b437570d0349c6eaa
Shubham-tiwari123 <st540362@gmail.com>
2019-09-19 12:52:57 +0530

    Game file

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,10 +1,12 @@
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -12,8 +14,8 @@ import javax.swing.Timer;
 
 public class GamePlay extends JPanel implements KeyListener,ActionListener{
 
-    private int[] snakeXLength = new int[750];
-    private int[] snakeYLength = new int[750];
+    private final int[] snakeXLength = new int[750];
+    private final int[] snakeYLength = new int[750];
     
     private boolean left =false;
     private boolean right =false;
@@ -26,12 +28,25 @@ public class GamePlay extends JPanel implements KeyListener,ActionListener{
     private ImageIcon leftmouth;
     
     private int snakeLen =3;
-    private Timer timer;
-    private int delay = 100;
+    private final Timer timer;
+    private final int delay = 100;
     private  int moves=0;
+    private int score=0;
     
     private ImageIcon snakeImage;
     private ImageIcon titleImage;
+    private ImageIcon enemyImage;
+    
+    
+    
+    private final int[] enemyXPos={25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,
+                            525,550,575,600,625,650,675,700,725,750,775,800,825,850};
+    private final int[] enemyYPos={75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,
+                            525,550,575,600,625};
+    
+    private final Random random = new Random();
+    private int xpos = random.nextInt(34);
+    private int ypos = random.nextInt(23);
     
     public GamePlay() {
         addKeyListener(this);
@@ -64,6 +79,16 @@ public class GamePlay extends JPanel implements KeyListener,ActionListener{
         g.setColor(Color.black);
         g.fillRect(25, 75, 850, 575);
         
+        //score
+        g.setColor(Color.white);
+        g.setFont(new Font("arial",Font.PLAIN, 14));
+        g.drawString("Score: "+score, 780, 30);
+        
+        //len of snake
+        g.setColor(Color.white);
+        g.setFont(new Font("arial",Font.PLAIN, 14));
+        g.drawString("Length: "+snakeLen, 780, 50);
+        
         rightmouth = new ImageIcon("/home/shubham/NetBeansProjects/SnakeGame/src/image/rightmouth.png");
         rightmouth.paintIcon(this, g, snakeXLength[0], snakeYLength[0]);
         
@@ -90,26 +115,164 @@ public class GamePlay extends JPanel implements KeyListener,ActionListener{
             }
         }
         
+        enemyImage = new ImageIcon("/home/shubham/NetBeansProjects/SnakeGame/src/image/enemy.png");
+        if(enemyXPos[xpos]==snakeXLength[0] && enemyYPos[ypos]==snakeYLength[0]){
+            snakeLen++;
+            score++;
+            xpos = random.nextInt(34);
+            ypos = random.nextInt(23);
+        }
+        enemyImage.paintIcon(this, g, enemyXPos[xpos], enemyYPos[ypos]);
+        
+        for(int i=1;i<snakeLen;i++){
+            if(snakeXLength[i]==snakeXLength[0] && snakeYLength[i]==snakeYLength[0]){
+                right=false;
+                left=false;
+                up=false;
+                down=false;
+                g.setColor(Color.white);
+                g.setFont(new Font("arial",Font.BOLD, 50));
+                g.drawString("Game over:", 300, 300);
+                
+                g.setColor(Color.white);
+                g.setFont(new Font("arial",Font.BOLD, 20));
+                g.drawString("Press space bar RESTRAT:", 350, 340);
+            }
+        }
         g.dispose();
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            moves =0;
+            score=0;
+            snakeLen=3;
+            repaint();
+        }
+        
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            moves++;
+            right = true;
+            if(!left)
+                right=true;
+            else{
+                right=false;
+                left=true;
+            }
+            up=false;
+            down=false;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            moves++;
+            left = true;
+            if(!right)
+                left=true;
+            else{
+                left=false;
+                right=true;
+            }
+            up=false;
+            down=false;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            moves++;
+            up = true;
+            if(!down)
+                up=true;
+            else{
+                up=false;
+                down=true;
+            }
+            right=false;
+            left=false;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            moves++;
+            down = true;
+            if(!up)
+                down=true;
+            else{
+                down=false;
+                up=true;
+            }
+            right=false;
+            left=false;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        timer.start();
+        if(right){
+            for(int i=snakeLen-1;i>=0;i--){
+                snakeYLength[i+1] = snakeYLength[i];
+            }
+            for(int i=snakeLen;i>=0;i--){
+                if(i==0){
+                    snakeXLength[i] = snakeXLength[i]+25;
+                }
+                else
+                    snakeXLength[i]= snakeXLength[i-1];
+                if(snakeXLength[i]>850)
+                    snakeXLength[i]=25;
+            }
+            repaint();
+        }
+        if(left){
+            for(int i=snakeLen-1;i>=0;i--){
+                snakeYLength[i+1] = snakeYLength[i];
+            }
+            for(int i=snakeLen;i>=0;i--){
+                if(i==0){
+                    snakeXLength[i] = snakeXLength[i]-25;
+                }
+                else
+                    snakeXLength[i]= snakeXLength[i-1];
+                if(snakeXLength[i]<25)
+                    snakeXLength[i]=850;
+            }
+            repaint();
+        }
+        if(up){
+            for(int i=snakeLen-1;i>=0;i--){
+                snakeXLength[i+1] = snakeXLength[i];
+            }
+            for(int i=snakeLen;i>=0;i--){
+                if(i==0){
+                    snakeYLength[i] = snakeYLength[i]-25;
+                }
+                else
+                    snakeYLength[i]= snakeYLength[i-1];
+                if(snakeYLength[i]<75)
+                    snakeYLength[i]=625;
+            }
+            repaint();
+        }
+        if(down){
+            for(int i=snakeLen-1;i>=0;i--){
+                snakeXLength[i+1] = snakeXLength[i];
+            }
+            for(int i=snakeLen;i>=0;i--){
+                if(i==0){
+                    snakeYLength[i] = snakeYLength[i]+25;
+                }
+                else
+                    snakeYLength[i]= snakeYLength[i-1];
+                if(snakeYLength[i]>625)
+                    snakeYLength[i]=75;
+            }
+            repaint();
+        }
     }
 }
